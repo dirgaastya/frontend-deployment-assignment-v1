c5dc6f08718938c9b66a8fb374df8a4884c7c9bb
dirgaastya <astya444@gmail.com>
2022-12-14 15:13:03 +0700

    initial commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "DIRGA ASTYA WISNUWARDANA" // TODO: replace this
+const studentId = "FE2583441" // TODO: replace this
+
+export { url, studentName, studentId }
